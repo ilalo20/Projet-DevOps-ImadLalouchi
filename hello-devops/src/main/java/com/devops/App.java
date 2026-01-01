2aba70027b3f60367c45cd7c826eeb3489219d9d
ilalo20 <iimadeae@gmail.com>
2026-01-01 16:09:51 +0100

    Ajout application Java DevOps

---
+++ hello-devops/src/main/java/com/devops/App.java
@@ -0,0 +1,14 @@
+package com.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Projet Dev-Ops" );
+    }
+}
